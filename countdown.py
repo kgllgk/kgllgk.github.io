7130262b1312bf74d43fb6d0331290efdf45a667
kglatom <03.sprier-ibis@icloud.com>
2022-07-17 01:19:19 -0700

    final commit for the night, all bugs squashed

--- countdown.py
+++ countdown.py
@@ -13,12 +13,12 @@ while True:
         else:
             daystxt = " days, "
         hours = delta.seconds / 3600
-        if hours == 1:
+        if math.floor(hours) == 1:
             hourstxt = " hour, "
         else:
             hourstxt = " hours, "
         minutes = ((delta.seconds / 3600) % 1) * 60
-        if minutes == 1:
+        if math.floor(minutes) == 1:
             minutestxt = " minute, and "
         else:
             minutestxt = " minutes, and "
@@ -32,12 +32,32 @@ while True:
         time.sleep(1)
 
 # debugging area
-# for i in range(60):
-#     delta = datetime.datetime(2022, 7, 20) - datetime.datetime.now()
+# for i in range(10):
+#     delta = datetime.datetime(2022, 7, 18, 2,18,5) - datetime.datetime.now()
+#     if delta.days == 1:
+#         daystxt = " day, "
+#     else:
+#         daystxt = " days, "
 #     hours = delta.seconds / 3600
+#     if math.floor(hours) == 1:
+#         hourstxt = " hour, "
+#         print(f"act {hours} floor {math.floor(hours)} ceil {math.ceil(hours)}")
+#     else:
+#         hourstxt = " hours, "
+#         print(f"act {hours} floor {math.floor(hours)} ceil {math.ceil(hours)}")
+
 #     minutes = ((delta.seconds / 3600) % 1) * 60
+#     if math.floor(minutes) == 1:
+#         minutestxt = " minute, and "
+#     else:
+#         minutestxt = " minutes, and "
 #     seconds = (minutes % 1) * 60
-#     print("floor: ", round(seconds), "seconds | act: ", seconds)
+#     if round(seconds) == 1:
+#         secondstxt = " second."
+#     else:
+#         secondstxt = " seconds."
+#     x = str(delta.days) + daystxt + str(math.floor(hours)) + hourstxt + str(math.floor(minutes)) + minutestxt + str(round(seconds)) + secondstxt
+#     print(f"x = {x}")
 #     time.sleep(1)
 #     i+=1
 
